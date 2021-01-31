c846c439bc84c7440164d63ec4178f395d3e4c57
Kayson Langford <10740041@my.uvu.edu>
2021-01-31 16:19:39 -0700

    Final Changes to CameraFollow script.

--- prototype_1/Assets/Scripts/CameraFollow.cs
+++ prototype_1/Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0,7,-11);
+    public Vector3 offset = new Vector3(0,7,-11);
     // Update is called once per frame
     void Update()
     {
--- prototype_1/Assets/Scripts/Move.cs
+++ prototype_1/Assets/Scripts/Move.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
   // Access Modifier, Data Type, Name
-  private float speed = 30.0f;
-  private float turnspeed = 35.0f;
-  private float hInput;
-  private float fInput;
+  public float speed = 30.0f;
+  public float turnspeed = 35.0f;
+  public float hInput;
+  public float fInput;
     // Update is called once per frame
     void Update()
     {
       hInput = Input.GetAxis("Horizontal");
       fInput = Input.GetAxis("Vertical");
       transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput); 
-      transform.Rotate(Vector3.up, turnSpeed * hInput * Time.deltaTime); 
+      transform.Rotate(Vector3.up, turnspeed * hInput * Time.deltaTime); 
     }
 }
